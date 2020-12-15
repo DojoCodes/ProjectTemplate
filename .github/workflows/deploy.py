0e1a8fb08d34765eb09cf008a2f2082d831d1293
Sygmei <>
2020-12-15 13:40:29 +0100

    Add YAML Loader

--- .github/workflows/deploy.py
+++ .github/workflows/deploy.py
@@ -5,7 +5,7 @@ def create_or_update_challenge(challenge_path: str):
     print(f"Loading challenge definition at '{challenge_path}'")
 
 
-project = yaml.load("project.yml")
+project = yaml.load("project.yml", Loader=yaml.FullLoader)
 
 for challenge in project["challenges"]:
     create_or_update_challenge(challenge)
